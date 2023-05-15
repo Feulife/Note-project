d55412b6eef6776f01f1e8c65fe606e845258880
Andrii Racilewicz <75903440+Feulife@users.noreply.github.com>
2023-05-15 22:36:34 +0200

    add like symbol, improved dark theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Row, Col, Table, Button, Card, Form } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -23,7 +22,7 @@ function App() {
   });
 
   const [currentId, setCurrentId] = useState(0);
- 
+
   useEffect(() => {
     try {
       dispatch(getNotes());
@@ -46,7 +45,7 @@ function App() {
     setCurrentId(0);
     setNoteData({
       title: "",
-      description: "",      
+      description: "",
       dateCreate: "",
       like: false,
     });
@@ -62,135 +61,146 @@ function App() {
       clear();
     }
   };
- 
+
   return (
     <div className="App">
-      <div className="section" style={{ margin: "20px" }}>
-        <Row>
-          <Col md={4}>
-            <Card bg="light">
-              <Card.Body>
-                <Card.Title>NOTE Form</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  {currentId ? "Edit" : "Create"} NOTE
-                </Card.Subtitle>
-                <Form onSubmit={handleSubmit}>
-                  <Form.Group className="mb-3" controlId="formGroupTitle">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control
-                      name="title"
-                      type="text"
-                      placeholder="Enter Title"
-                      value={noteData.title}
-                      onChange={(e) =>
-                        setNoteData({
-                          ...noteData,
-                          title: e.target.value,
-                        })
-                      }
-                    />
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="formGroupDescription">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                      name="description"
-                      type="text"
-                      placeholder="Enter Description"
-                      value={noteData.description}
-                      onChange={(e) =>
-                        setNoteData({
-                          ...noteData,
-                          description: e.target.value,
-                        })
-                      }
-                    />
-                  </Form.Group>
-                  <div className="d-grid gap-2">
-                    <Button
-                      variant="success"
-                      size="lg"
-                      type="submit"
-                      onClick={() =>
-                        setNoteData({
-                          ...noteData,
-                          dateCreate: showDateTime,
-                        })
-                      }
+      <div className="section" style={{ margin: "10px"}}>
+          <div class="position-fixed" style={{ marginLeft: "1%", width: "500px" }}>
+            <Col md={12}>
+              <Card bg="dark">
+                <Card.Body>
+                  <Card.Title className="text-center text-light">
+                    PROVERB Form
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2 text-center text-muted">
+                    {currentId ? "Edit" : "Create"} PROVERB
+                  </Card.Subtitle>
+                  <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formGroupTitle">
+                      <Form.Label className="text-light">Title</Form.Label>
+                      <Form.Control
+                        name="title"
+                        type="text"
+                        placeholder="Enter Title"
+                        value={noteData.title}
+                        onChange={(e) =>
+                          setNoteData({
+                            ...noteData,
+                            title: e.target.value,
+                          })
+                        }
+                      />
+                    </Form.Group>
+                    <Form.Group
+                      className="mb-3"
+                      controlId="formGroupDescription"
                     >
-                      Save
-                    </Button>
-                    <Button variant="secondary" size="lg" onClick={clear}>
-                      Cancel
-                    </Button>
-                  </div>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={8}>
-            <Table striped bordered hover variant="dark">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Title</th>
-                  <th>Description</th>
-                  <th>Published</th>
-                  <th>Like</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {notes.map((note, index) => (
-                  <>
-                    <tr key={index}>
-                      <td>{index + 1}</td>
-                      <td>{note?.title}</td>
-                      <td>{note?.description}</td>
-                      <td>{note.dateCreate}</td>
-                      <td>{note?.like ? "I like it" : "I dislike it"}</td>
-                      <td>
-                        <div className="d-grid gap-2">
-                          <Button
-                            variant="outline-info"
-                            size="sm"
-                            style={{ margin: "1%" }}                            
-                            onClick={() => {                              
-                              dispatch(likeNote(note._id, {
-                                title: note.title,
-                                description: note.description,
-                                dateCreate: note.dateCreate,
-                                like: !note.like,
-                              })
-                              );
-                            }}
-                          >
-                            {note?.like ? "Not Like" : "like"}
-                          </Button>                  
-                          <Button
-                            variant="outline-warning"
-                            size="sm"
-                            style={{ margin: "1%" }}
-                            onClick={() => setCurrentId(note._id)}
-                          >
-                            Update
-                          </Button>
-                          <Button
-                            variant="outline-danger"
-                            size="sm"
-                            style={{ margin: "1%" }}
-                            onClick={() => dispatch(deleteNote(note._id))}
-                          >
-                            Delete
-                          </Button>
-                        </div>
-                      </td>
-                    </tr>
-                  </>
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
+                      <Form.Label className="text-light">
+                        Description
+                      </Form.Label>
+                      <Form.Control
+                        name="description"
+                        type="text"
+                        placeholder="Enter Description"
+                        value={noteData.description}
+                        onChange={(e) =>
+                          setNoteData({
+                            ...noteData,
+                            description: e.target.value,
+                          })
+                        }
+                      />
+                    </Form.Group>
+                    <div className="d-grid gap-2">
+                      <Button
+                        variant="success"
+                        size="lg"
+                        type="submit"
+                        onClick={() =>
+                          setNoteData({
+                            ...noteData,
+                            dateCreate: showDateTime,
+                          })
+                        }
+                      >
+                        Save
+                      </Button>
+                      <Button variant="secondary" size="lg" onClick={clear}>
+                        Cancel
+                      </Button>
+                    </div>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          </div>
+        
+          <div style={{ marginLeft: "30%" }}>
+            <Col md={10}>
+              <Table striped bordered hover variant="dark">
+                <thead>
+                  <tr class="sticky-top">
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Published</th>
+                    <th>Like</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {notes.map((note, index) => (
+                    <>
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{note?.title}</td>
+                        <td>{note?.description}</td>
+                        <td>{note.dateCreate}</td>
+                        <td>{note?.like ? `\u261D` : " "}</td>
+                        <td>
+                          <div className="d-grid gap-2">
+                            <Button
+                              variant="outline-info"
+                              size="sm"
+                              style={{ margin: "1%" }}
+                              onClick={() => {
+                                dispatch(
+                                  likeNote(note._id, {
+                                    title: note.title,
+                                    description: note.description,
+                                    dateCreate: note.dateCreate,
+                                    like: !note.like,
+                                  })
+                                );
+                              }}
+                            >
+                              {note?.like ? "Dislike" : "like"}
+                            </Button>
+                            <Button
+                              variant="outline-warning"
+                              size="sm"
+                              style={{ margin: "1%" }}
+                              onClick={() => setCurrentId(note._id)}
+                            >
+                              Update
+                            </Button>
+                            <Button
+                              variant="outline-danger"
+                              size="sm"
+                              style={{ margin: "1%" }}
+                              onClick={() => dispatch(deleteNote(note._id))}
+                            >
+                              Delete
+                            </Button>
+                          </div>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </Table>
+            </Col>
+          </div>
       </div>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
@@ -19,5 +18,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-reportWebVitals();
