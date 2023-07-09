77a7c55c9a9652f5b5a4e44a8500887050be4e2f
Andrii Racilewicz <75903440+Feulife@users.noreply.github.com>
2023-07-09 21:37:04 +0200

    changed base URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "http://note-project-backend.vercel.app"
 })
 
 export const fetchNotes = () => API.get(`/note`);
